8259c29744fdcbb854a9a7ce374f3629b5e34adb
seralvic6 <sergio.alonsov@alumnos.upm.es>
2025-02-18 18:29:36 +0100

    Week 3(unfinished) First commit with OSM activity.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+    implementation("org.osmdroid:osmdroid-android:6.1.11")
 }
---
+++ app/src/main/java/com/ProyectoMAD/OpenStreetMapsActivity.kt
@@ -0,0 +1,53 @@
+package com.ProyectoMAD
+
+import android.location.Location
+import android.util.Log
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+
+
+class OpenStreetMapsActivity : AppCompatActivity() {
+    private val TAG = "btaOpenStreetMapActivity"
+    private lateinit var map: MapView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.d(TAG, "onCreate: Starting activity...");
+
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_open_street_maps)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Configuration.getInstance().userAgentValue = "com.ProyectoMAD"
+        Configuration.getInstance().load(applicationContext, getSharedPreferences("osm", MODE_PRIVATE))
+
+        val bundle = intent.getBundleExtra("locationBundle")
+        val location: Location? = bundle?.getParcelable("location")
+
+        val startPoint = if (location != null) {
+            Log.d(TAG, "onCreate: Location[${location.altitude}][${location.latitude}][${location.longitude}]")
+            GeoPoint(location.latitude, location.longitude)
+        } else {
+            Log.d(TAG, "onCreate: Location is null, using default coordinates")
+            GeoPoint(40.389683644051864, -3.627825356970311)
+        }
+
+        map = findViewById(R.id.map)
+        map.setTileSource(TileSourceFactory.MAPNIK)
+        map.controller.setZoom(15.0)
+        map.controller.setCenter(startPoint)
+
+    }
+}
