a060cb1a23d3d4e06d3fa2d3ee4832f8ba0a9870
seralvic6 <sergio.alonsov@alumnos.upm.es>
2025-02-26 17:46:59 +0100

    Week 4(unfinished)
    Fixed: Week 3 markers related to our final app.

--- app/src/main/java/com/ProyectoMAD/OpenStreetMapsActivity.kt
+++ app/src/main/java/com/ProyectoMAD/OpenStreetMapsActivity.kt
@@ -32,6 +32,42 @@ class OpenStreetMapsActivity : AppCompatActivity() {
         GeoPoint(40.39037466191718, -3.6270256763598447), // Library
         GeoPoint(40.389855884803005, -3.626782180787362) // CITSEM
     )
+    val retiroMarkers = listOf(
+        GeoPoint(40.42117535604317, -3.680401468693263), //Montaña artificial
+        GeoPoint(40.42061417242487, -3.6810108904829124), //Casita del Pescador
+        GeoPoint(40.418760474710574, -3.685308337528424), //Fuente de los Galápagos
+        GeoPoint(40.41948863633951, -3.6872932972242913), //Paseo de México
+        GeoPoint(40.41525783875198, -3.687802338144712), //Jardín del Parterre
+        GeoPoint(40.4171343534008, -3.683909791107988), //Estanque Grande del Retiro
+        GeoPoint(40.41209021987538, -3.6871366611675906), //Bosque del Recuerdo
+        GeoPoint(40.40838225487468, -3.678519994560459), //Puerta de Mariano de Cavia
+        GeoPoint(40.41079474417153, -3.680315379632557), //La Rosaleda (Cecilio Rodríguez)
+        GeoPoint(40.41359592139659, -3.6819739132752844), //Palacio de Cristal
+        GeoPoint(40.41330744341854, -3.678005171232507), //Jardines de Cecilio Rodríguez
+        GeoPoint(40.415571513393296, -3.6787337586579363) //Jardines de Herrero de Palacios
+    )
+    val retiroNames = listOf(
+        "Montaña artificial",
+        "Casita del Pescador",
+        "Fuente de los Galápagos",
+        "Paseo de México",
+        "Jardín del Parterre",
+        "Estanque Grande del Retiro",
+        "Bosque del Recuerdo",
+        "Puerta de Mariano de Cavia",
+        "La Rosaleda (Cecilio Rodríguez)",
+        "Palacio de Cristal",
+        "Jardines de Cecilio Rodríguez",
+        "Jardines de Herrero de Palacios"
+    )
+    val berroMarkers = listOf(
+        GeoPoint(40.422598290025114, -3.6627296111863354), //Centro cultural Quinta del Berro
+        GeoPoint(40.42207086085666, -3.6602394414882493) //Ginko Biloba
+    )
+    val berroNames = listOf(
+        "Centro cultural Quinta del Berro",
+        "Ginko Biloba"
+    )
     val gymkhanaNames = listOf(
         "Tennis",
         "Futsal outdoors",
@@ -94,6 +130,8 @@ class OpenStreetMapsActivity : AppCompatActivity() {
         map.overlays.add(gymkhanaMarker)
 
         addGymkhanaMarkers(map, gymkhanaCoords, gymkhanaNames, this)
+        addGymkhanaMarkers(map, retiroMarkers, retiroNames, this)
+        addGymkhanaMarkers(map, berroMarkers, berroNames, this)
 
         val routeMarker = Marker(map)
         routeMarker.position = startPoint
@@ -103,6 +141,7 @@ class OpenStreetMapsActivity : AppCompatActivity() {
         map.overlays.add(routeMarker)
 
         addRouteMarkers(map, gymkhanaCoords, gymkhanaNames, this)
+        addRouteMarkers(map, berroMarkers, berroNames, this)
 
         map.controller.setZoom(18.0)
     }
